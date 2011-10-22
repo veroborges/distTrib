7f52dfda4df4452c99fc876a295f37f905c90668
Anar Huseynov <ahuseyno@andrew.cmu.edu>
2011-10-22 14:21:11 -0400

    Added register rpc functionality

--- storageserver/storageserver.go
+++ storageserver/storageserver.go
@@ -4,34 +4,65 @@ import (
 	"storageproto"
 	"rpc"
 	"storage"
-	"storagerpc"
 	"os"
 	"time"
+	"sync"
+	"net"
+	"log"
+	"fmt"
 )
 
 type Storageserver struct {
-	tm *TribMap
-	servers []*serverData
+	tm *storage.TribMap
+	servers []serverData
 	numnodes int
+	nodeid uint32
+	cond *sync.Cond
 }
 
-type serverData {
-	server *rpc.Client
-	nodeid uint32
+type serverData struct {
+	rpc *rpc.Client
+	clientInfo storageproto.Client
 }
 
 func NewStorageserver(master string, numnodes int, portnum int, nodeid uint32) *Storageserver {
-	ss := &Storageserver{storage.NewTribMap(), make([]*rpc.Client, 0, 0), numnodes}
+	ss := &Storageserver{storage.NewTribMap(), nil, numnodes, nodeid, nil}
+	var lock sync.Mutex
+	ss.cond = sync.NewCond(lock)
+	socket := net.JoinHostPort(master, fmt.Sprintf("%d", portnum))
 	if (numnodes == 0) {
 		for {
-			master, err := rpc.DialHTTP("tcp", net.JoinHostPort(master, portnum))
+			master, err := rpc.DialHTTP("tcp", socket)
 			if err != nil {
-				log.Info("Failed to connect to the master; retrying")
+				log.Printf("Failed to connect to the master; retrying")
 				time.Sleep(1000000000)
 			} else {
 				reply := new(storageproto.RegisterReply)
-				master.Call("StorageRPC.Register", 
+				err = master.Call("StorageRPC.Register",&RegisterArgs{Client{socket, nodeid}}, reply)
+				if (err != nil) {
+					log.Printf("Something went wrong with the call; retrying")
+				} else if reply.Ready {
+					ss.servers = make([]serverData, serverlen(reply.Clients))
+					for i:=0; i < len(reply.Clients); i++ {
+						ss.servers[i] = &serverData{nil, reply.Clients[i]}
+					}
+					break
+				}
+				time.Sleep(1000000000)
 			}
+		}
+	} else {
+		ss.servers = make([]serverData, numnodes)
+		ss.cond.L.Lock()
+		for numnodes != 0 {
+			ss.cond.Wait()
+		}
+		ss.cond.Unlock()
+	}
+	for i:=0; i < len(reply.Clients); i++ {
+		ss.servers[i].rpc, err := rpc.DialHTTP("tcp", ss.servers[i].clientInfo.HostPort)
+		if err != nil {
+			log.Fatal("Problem creating rpc connection to %s", ss.servers[i].clientInfo.HostPort)
 		}
 	}
 	return ss
@@ -47,6 +78,20 @@ func NewStorageserver(master string, numnodes int, portnum int, nodeid uint32) *
 // These should do something! :-)
 
 func (ss *Storageserver) RegisterRPC(args *storageproto.RegisterArgs, reply *storageproto.RegisterReply) os.Error {
+	ss.cond.L.Lock()
+	if (numnodes > 0) {
+		ss.servers[numnodes - 1].clientInfo = args.ClientInfo
+		numnodes--
+	}
+	reply.Ready = false
+	if (numnodes == 0) {
+		ss.cond.Broadcast()
+		reply.Ready = true
+		for i:=0; i < len(ss.servers); i++ {
+			reply.Clients[i] = ss.servers[i].clientInfo
+		}
+	}
+	ss.cond.Unlock()
 	return nil
 }
 
